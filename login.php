75881aa9463ef78454318e035aac91d02a47ac0e
Arka Ghosh Chowdhury <arkaghoshchowdhury@gmail.com>
2022-07-12 22:51:39 +0530

    Update

--- login.php
+++ login.php
@@ -5,92 +5,26 @@ $msg=$_GET['msg'];
 <html lang="en-US">
     <head>
         <link rel="icon" href="images/favicon.ico">
-        <title>User Login | College Campus</title>
+        <title>College Campus</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--CSS-->
         <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <!--JS-->
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
         <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <!--Google Fonts-->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <style>
-            #background{
-                background:url("images/bg3.jpg");
-                width: 100%;
-            }
-            /* BUTTON STYLE */
-            .btn_confirm{
-                border-radius: 4px;
-                background-color: #33adff;
-                border: none;
-                color: #FFFFFF;
-                text-align: center;
-                font-size: 20px;
-                padding: 6px;
-                width: 100px;
-                transition: all 0.5s;
-                cursor: pointer;
-                margin: 5px;
-                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-            }
-            .btn_confirm span {
-                cursor: pointer;
-                display: inline-block;
-                position: relative;
-                transition: 0.5s;
-            }
-            .btn_confirm span:after {
-                content: '\00bb';
-                position: absolute;
-                opacity: 0;
-                top: 0;
-                right: -20px;
-                transition: 0.5s;
-            }
-            .btn_confirm:hover span {
-                padding-right: 25px;
-            }
-            .btn_confirm:hover span:after {
-                opacity: 1;
-                right: 0;
-            }
-            .btn_confirm:hover {
-                box-shadow: 0 8px 16px 0 rgba(0,0,0,0.4), 0 6px 20px 0 rgba(0,0,0,0.19);
-            }
-            /* MODAL STYLE SHEET */ 
-            .modal.fade .modal-dialog {
-                -webkit-transform: scale(0.1);
-                -moz-transform: scale(0.1);
-                -ms-transform: scale(0.1);
-                transform: scale(0.1);
-                top: 400px;
-                opacity: 0;
-                -webkit-transition: all 0.3s;
-                -moz-transition: all 0.3s;
-                transition: all 0.3s;
-            }
-            .modal.fade.in .modal-dialog {
-                -webkit-transform: scale(1);
-                -moz-transform: scale(1);
-                -ms-transform: scale(1);
-                transform: scale(1);
-                -webkit-transform: translate3d(0, -300px, 0);
-                transform: translate3d(0, -300px, 0);
-                opacity: 1;
-            }
-            .modal-header {
-                font-family: "Bree Serif";
-                background-color: #F6F5F5;
-                border-radius: 5px;
-            }
-            .modal-body {
-                font-size: 16px;
-            }
-            p {
-    margin: 50px;
+            p{
+                margin: 50px;
             }
         </style>
         <script>
@@ -99,40 +33,35 @@ $msg=$_GET['msg'];
                 uid.value = uid.value.toUpperCase();
             }
             // Hiding Default Context Menu
-        document.addEventListener('contextmenu', event => event.preventDefault());
+            document.addEventListener('contextmenu', event => event.preventDefault());
         </script>
     </head>
     <body id="background">
         <div class="container">
-        <nav class="navbar navbar-inverse navbar-fixed-top" id="myTopnav"
-         style="background-color:rgb(51,51,51); 
-                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.3);">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                 <a class="navbar-brand" href="index.php" style="font-size: 20px; color:rgb(255,255,255)"><img src="images/favicon_32X32.ico"></a>
-                <a class="navbar-brand" 
-                   href="index.php" 
-                   style="font-size: 20px; 
-                          color:rgb(255,255,255)">CCampus</a>
-            </div>
-            <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li><a style="font-size: 20px;" href="about_us.php">About Us</a></li>
-                <li><a style="font-size: 20px;" href="contact_us.php">Contact Us</a></li>
-                <li><a style="font-size: 20px;" href="help.php">Help</a></li>
-            </ul>
-                <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="registration.php"><span class="glyphicon glyphicon-edit"></span> Sign Up</a></li>
-            </div>
-            </ul>
-        </div>
-    </nav>
+            <nav class="navbar navbar-inverse navbar-fixed-top" id="myTopnav" style="background-color:rgb(51,51,51); box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.3);">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle Navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index.php" style="font-size: 20px; color:rgb(255,255,255)"><img src="images/favicon_32X32.ico"></a>
+                        <a class="navbar-brand" href="index.php" style="font-size: 20px; color:rgb(255,255,255)">CCampus</a>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li><a style="font-size: 20px;" href="about_us.php">About Us</a></li>
+                            <li><a style="font-size: 20px;" href="contact_us.php">Contact Us</a></li>
+                            <li><a style="font-size: 20px;" href="help.php">Help</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active"><a href="registration.php"><span class="glyphicon glyphicon-edit"></span> Sign Up</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         
             <div id="loginbox" style="margin-top:100px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8  col-sm-offset-2">                    
                 <div class="panel panel-info" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0,0,0,0.5);">
@@ -202,6 +131,7 @@ $msg=$_GET['msg'];
                 </div>                     
             </div>  
         </div>
+        
         <!--- Alert Box -->
         <div class="container">
         <div class="row" style="margin-top: 20px">
