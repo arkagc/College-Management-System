f12170123a052d6fc069843026270830e4816e5e
Arka Ghosh Chowdhury <arkaghoshchowdhury@gmail.com>
2022-07-14 10:49:11 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-$msg=$_GET['msg'];
+    $msg='';
 ?>
 <!DOCTYPE html>
 <html lang="en-US">
@@ -13,6 +13,7 @@ $msg=$_GET['msg'];
         <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
 
         <!--JS-->
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
@@ -134,26 +135,24 @@ $msg=$_GET['msg'];
         
         <!--- Alert Box -->
         <div class="container">
-        <div class="row" style="margin-top: 20px">
-            <div class="col-sm-3"></div>
-            <div class="col-sm-6">
-                <?php if($msg!=''){?>
-    <div class="alert alert-danger alert-dismissible fade in"  id="inCorrectUserIdPassword">
-    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-    <span><center><?php echo $msg; ?></center></span>
-  </div>
-<?php } ?>
-            
-        </div>
-        <div class="col-sm-3"></div>
-        </div>
+            <div class="row" style="margin-top: 20px">
+                <div class="col-sm-3"></div>
+                <div class="col-sm-6">
+                    <?php if($msg!=''){?>
+                        <div class="alert alert-danger alert-dismissible fade in"  id="inCorrectUserIdPassword">
+                            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                            <span><center><?php echo $msg; ?></center></span>
+                        </div>
+                    <?php } ?>
+                </div>
+                <div class="col-sm-3"></div>
+            </div>
         </div>
         
-        </div>
+        <!-- </div> -->
     
-    <div class="col-sm-12">
-    <p style="font-size: 15px; text-align: center">Designed & Developed by Arka Ghosh Chowdhury&nbsp;</p>
+        <div class="col-sm-12">
+            <p style="font-size: 15px; text-align: center">Designed & Developed by Arka Ghosh Chowdhury&nbsp;</p>
         </div>
-        
     </body>
 </html>
