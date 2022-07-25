fdbe45859c420c5aafc7d805f97fdc4bc9fe0249
Arka Ghosh Chowdhury <arkaghoshchowdhury@gmail.com>
2022-07-25 10:43:37 +0530

    Update

--- DBconnect.php
+++ DBconnect.php
@@ -8,7 +8,6 @@
             $password = "";
             $dbname = "college_campus";
             $conn = new mysqli($servername,$username,$password,$dbname);
-            // Check for database connection error
             if (mysqli_connect_errno()) {
                 echo "Failed to connect to MySQL: " . mysqli_connect_error();
                 exit;
--- autosuggestuser.php
+++ autosuggestuser.php
@@ -3,41 +3,30 @@
     $db = new DBconnect();
     $conn=$db->connect();
 
-if(isset($_POST['item']) && !empty($_POST['item']))  // Page validation against empty field
-{
-	// This is the item to search for in the database
-	$this_item = trim(strip_tags($_POST['item']));
-	//echo $this_item;
-if($this_item == "") 
+	if(isset($_POST['item']) && !empty($_POST['item'])) 
 	{
-		
-		echo '<div class="un_suggested_items">There was no data passed</div>';
-	}
-	else
-	{
-		$select= "select user_id, CONCAT(first_name, ' ', middle_name,' ', last_name) as username from user where first_name like '".($this_item)."%' order by first_name asc";
-		//echo $select;
-		//die;
-		$check_item = mysqli_query($conn,$select);
-		if(mysqli_num_rows($check_item) > 0) 
+		$this_item = trim(strip_tags($_POST['item']));
+		if($this_item == "") 
 		{
-			
-			echo '<div style="border:solid 1px;height:200px;width:560px;overflow-y:scroll;">';
-			
-			while ($get_item = mysqli_fetch_array($check_item)) 
-			{
-				$user = trim($get_item["user_id"]);
-                //echo $user;
-				echo '<div id="user_name"><a href="profile_member.php?username='.$user.'">'.$get_item["username"].'</a></div>';
-			}
-			
-			//echo '</div>';
-		} 
-		else 
-		{
-			
-			echo '<div class="un_suggested_items">Sorry, there was no result found.</div>';
+			echo '<div class="un_suggested_items">There was no data passed</div>';
 		}
-	}  
-} 
+		else
+		{
+			$select= "select user_id, CONCAT(first_name, ' ', middle_name,' ', last_name) as username from user where first_name like '".($this_item)."%' order by first_name asc";
+			$check_item = mysqli_query($conn,$select);
+			if(mysqli_num_rows($check_item) > 0) 
+			{
+				echo '<div style="border:solid 1px;height:200px;width:560px;overflow-y:scroll;">';
+				while ($get_item = mysqli_fetch_array($check_item)) 
+				{
+					$user = trim($get_item["user_id"]);
+					echo '<div id="user_name"><a href="profile_member.php?username='.$user.'">'.$get_item["username"].'</a></div>';
+				}
+			} 
+			else 
+			{
+				echo '<div class="un_suggested_items">Sorry, there was no result found.</div>';
+			}
+		}  
+	} 
 ?>
