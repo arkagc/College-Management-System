f3bab1af96b5bf343452640b513c45de21b01d1e
Arka Ghosh Chowdhury <arkaghoshchowdhury@gmail.com>
2022-07-31 19:29:22 +0530

    Update

--- home_process.php
+++ home_process.php
@@ -1,10 +1,8 @@
 <?php
      session_start();
-
-
-        if (!isset($_SESSION['user_id'])){
-        echo "<script>alert('You are not logged In!'); window.location='login.php';</script>";
-        }
+    if (!isset($_SESSION['user_id'])){
+    echo "<script>alert('You are not logged In!'); window.location='login.php';</script>";
+    }
 
     include 'DBlibrary.php';
     $db = new DBlibrary();
@@ -19,30 +17,30 @@
     $error=array();
     $extension=array("jpeg","jpg","png","gif");
     foreach($_FILES["files"]["tmp_name"] as $key=>$tmp_name)
+    {
+        $file_name=$_FILES["files"]["name"][$key];
+        $file_tmp=$_FILES["files"]["tmp_name"][$key];
+        $ext=pathinfo($file_name,PATHINFO_EXTENSION);
+        if(in_array($ext,$extension))
+        {
+            /* if(!file_exists("photo_gallery/".$txtGalleryName."/".$file_name))
             {
-                $file_name=$_FILES["files"]["name"][$key];
-                $file_tmp=$_FILES["files"]["tmp_name"][$key];
-                $ext=pathinfo($file_name,PATHINFO_EXTENSION);
-                if(in_array($ext,$extension))
-                {
-                   /* if(!file_exists("photo_gallery/".$txtGalleryName."/".$file_name))
-                    {
-                        move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$key],"photo_gallery/".$txtGalleryName."/".$file_name);
-                    }
-                    else
-                    {*/
-                  $filename=basename($file_name,$ext);
-                  $newFileName=$filename.time().".".$ext;
-                  //$img = $filename.time().".".$ext;
-                    $img=$img.";".$filename.time().".".$ext;
-                  move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$key],"postimage/".$newFileName);
-                 }
-                
-                else
-                {
-                    echo "invalid extension";	
-                }
+                move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$key],"photo_gallery/".$txtGalleryName."/".$file_name);
             }
+            else
+            {*/
+            $filename=basename($file_name,$ext);
+            $newFileName=$filename.time().".".$ext;
+            //$img = $filename.time().".".$ext;
+            $img=$img.";".$filename.time().".".$ext;
+            move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$key],"postimage/".$newFileName);
+            }
+        
+        else
+        {
+            echo "invalid extension";	
+        }
+    }
     
 /******************* POST VIDEO UPLOAD ************************/
     $allowedexts=array("mp4");
@@ -50,27 +48,24 @@
     $extension=end($temp);
     if($extension!=""){
         if(in_array($extension,$allowedexts) && ($_FILES["vfiles"]["size"]<20971520))
-    {
-        if($_FILES["vfiles"]["error"]>0)
         {
-            echo "Error uploading video";
+            if($_FILES["vfiles"]["error"]>0)
+            {
+                echo "Error uploading video";
+            }
+    
+            else
+            {
+                $newfilename = 'vid'.'_'.$user_id.'_'.round(microtime(true)) . '.' . $extension;
+                $vid = 'vid'.'_'.$user_id.'_'.round(microtime(true)) . '.' . $extension;
+                move_uploaded_file($_FILES["vfiles"]["tmp_name"],"postvideo/".$newfilename);
+            }
         }
-   
+    
         else
         {
-            $newfilename = 'vid'.'_'.$user_id.'_'.round(microtime(true)) . '.' . $extension;
-            $vid = 'vid'.'_'.$user_id.'_'.round(microtime(true)) . '.' . $extension;
-            move_uploaded_file($_FILES["vfiles"]["tmp_name"],"postvideo/".$newfilename);
-            
-            
-            
+            echo "Sorry! Invalid File";
         }
-    }
-    
-    else
-    {
-        echo "Sorry! Invalid File";
-    }
     }
 /********************* DOC FILES UPLOAD *************************/
     $allowedexts=array("pdf","txt","doc","docx",".zip",".rar");
@@ -78,31 +73,28 @@
     $extension=end($temp);
     if($extension!=""){
         if(in_array($extension,$allowedexts) && ($_FILES["docfiles"]["size"]<20971520))
-    {
-        if($_FILES["docfiles"]["error"]>0)
         {
-            echo "Error uploading video";
+            if($_FILES["docfiles"]["error"]>0)
+            {
+                echo "Error uploading video";
+            }
+    
+            else
+            {
+                $newfilename = 'doc'.'_'.$user_id.'_'.round(microtime(true)) . '.' . $extension;
+                $attch = 'doc'.'_'.$user_id.'_'.round(microtime(true)) . '.' . $extension;
+                move_uploaded_file($_FILES["docfiles"]["tmp_name"],"postdocs/".$newfilename);
+                
+                
+                
+            }
         }
-   
+    
         else
         {
-            $newfilename = 'doc'.'_'.$user_id.'_'.round(microtime(true)) . '.' . $extension;
-            $attch = 'doc'.'_'.$user_id.'_'.round(microtime(true)) . '.' . $extension;
-            move_uploaded_file($_FILES["docfiles"]["tmp_name"],"postdocs/".$newfilename);
-            
-            
-            
+            echo "Sorry! Invalid File";
         }
     }
-    
-    else
-    {
-        echo "Sorry! Invalid File";
-    }
-    }
-
-
     $result=$db->postUpload($user_id,$text_post,$img,$vid,$attch,$deptfilter,$status);
-
     header('Location: home_main.php');
 ?>
--- like.php
+++ like.php
@@ -1,6 +1,4 @@
 <?php
-//echo "text";
-//die;
 $server="localhost";
 $user="root";
 $pwd="";
@@ -9,15 +7,15 @@ $conn=new mysqli($server,$user,$pwd,$dbname);
 $post_id=$_POST['post_id'];
 $user_id=$_POST['user_id'];
 $stmt="INSERT INTO user_like (post_id,user_id,flag) VALUES ('$post_id','$user_id',1) ";
-        $result=mysqli_query($conn, $stmt);
-        if($result){
-            $stmt2="select count(*) as like_count from user_like where post_id='$post_id'";
-            $result2=mysqli_query($conn,$stmt2);
-            $row_count=mysqli_fetch_array($result2);
-            $count_like=$row_count['like_count'];
-            echo $count_like;
-        }
-        else{
-            "Error Clicking on Like Button !";
-        }
+$result=mysqli_query($conn, $stmt);
+if($result){
+    $stmt2="select count(*) as like_count from user_like where post_id='$post_id'";
+    $result2=mysqli_query($conn,$stmt2);
+    $row_count=mysqli_fetch_array($result2);
+    $count_like=$row_count['like_count'];
+    echo $count_like;
+}
+else{
+    "Error Clicking on Like Button !";
+}
 ?>
