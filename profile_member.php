2552ad77e671c7d93bcf65be0c2790afc09d065f
Arka Ghosh Chowdhury <arkaghoshchowdhury@gmail.com>
2022-08-20 09:47:55 +0530

    Update profile_member.php

--- profile_member.php
+++ profile_member.php
@@ -46,7 +46,7 @@
         background-color: dimgrey;
         background-image: url('images/search_icon.png');
         background-position: 6px 0px; 
-    background-repeat: no-repeat;
+        background-repeat: no-repeat;
     margin-top: 10px;
     margin-left: 15px;
     padding: 12px 20px 12px 40px;
