584b85e3bbf565b0c4e80e47501efb7d6d4c3fc8
Arka Ghosh Chowdhury <arkaghoshchowdhury@gmail.com>
2022-09-23 13:02:50 +0530

    Update profile_member.php

--- profile_member.php
+++ profile_member.php
@@ -78,7 +78,7 @@
     }
 
     img:hover {
-    box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
+        box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
 }
         /* Scroll bar style sheet*/
         /* width */
