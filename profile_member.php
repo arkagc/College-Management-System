6648fb94a252b7442ae6fa408415422ad38a4079
Arka Ghosh Chowdhury <arkaghoshchowdhury@gmail.com>
2023-01-05 11:04:10 +0530

    Update profile_member.php

--- profile_member.php
+++ profile_member.php
@@ -124,9 +124,9 @@
         padding: 8px;
         height: 50px;
         width: 50px;
-  border-radius: 50%;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.3);
-} 
+        border-radius: 50%;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.3);
+    } 
     #myBtn:hover {
   background-color: rgba(26, 144, 255, 0.8);
 }
