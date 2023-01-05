278d6493ff05b45ca9ebf55136f774efcce5555d
Arka Ghosh Chowdhury <arkaghoshchowdhury@gmail.com>
2023-01-05 11:03:50 +0530

    Update profile_member.php

--- profile_member.php
+++ profile_member.php
@@ -123,7 +123,7 @@
         cursor: pointer;
         padding: 8px;
         height: 50px;
-  width: 50px;
+        width: 50px;
   border-radius: 50%;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.3);
 } 
